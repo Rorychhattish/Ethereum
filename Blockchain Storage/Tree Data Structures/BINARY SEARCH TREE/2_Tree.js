a7d8a4b3e2bf9e323af6e7e41ced889034c98083
Chhatish kumar Sah <rorychhattish@gmail.com>
2026-08-11 14:40:00 +0545

    Complete Tree constructor

---
+++ Blockchain Storage/Tree Data Structures/BINARY SEARCH TREE/2_Tree.js
@@ -0,0 +1,16 @@
+// Goal: Store the Root
+// Finish the constructor function on the Tree class in the new file Tree.js.
+// All you need to do for now is store null on a root property.
+// const tree = new Tree();
+// console.log(tree.root); // null
+
+
+
+
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+}   
+module.exports = Tree;
